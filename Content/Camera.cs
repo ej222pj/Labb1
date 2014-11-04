73edcd8a3f7577c3f00b41613fd7757d0ff6509a
ej222pj <ej222pj@student.lnu.se>
2014-11-04 10:18:02 +0100

    Uppgift1 klar

--- Content/Camera.cs
+++ Content/Camera.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,16 @@ namespace Labb1.Content
 {
     class Camera
     {
-    }
+        int sizeOfTile = 64;
+        int borderSize = 64;
+        Vector2 vec;
+
+        public Vector2 getVisualX(Vector2 logical){
+            float x = borderSize + logical.X * sizeOfTile;
+            float y = borderSize + logical.Y * sizeOfTile;
+
+            return new Vector2(x, y);
+        }
+       
+    }                                       
 }
