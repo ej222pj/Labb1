0d96b5b860b0a0e6a7596a6967b89d8de42b7f96
ej222pj <ej222pj@student.lnu.se>
2014-11-04 11:12:59 +0100

    Uppgift3 Klar

--- Content/Camera.cs
+++ Content/Camera.cs
@@ -8,8 +8,28 @@ namespace Labb1.Content
 {
     class Camera
     {
-        int sizeOfTile = 64;
-        int borderSize = 64;
+        private int sizeOfTile = 64;
+        private int borderSize = 64;
+        private int levelWidth = 512;
+        private int levelHeight = 512;
+        private float scale;
+
+        public void reScale(int width/*320*/, int height/*240*/){
+            int scaleX = (width - borderSize * 2) / levelWidth;
+            int scaleY = (height - borderSize * 2) / levelHeight;
+
+            //Sätter skärmen efter vilken av X/Y som är minst
+            scale = scaleX;
+            if (scaleY < scaleX)
+            {
+                scale = scaleY;
+            }
+        }
+
+        public float getScale()
+        {
+            return scale;
+        }
 
         public Vector2 getVisualForWhite(Vector2 logicalPos){
             float x = borderSize + logicalPos.X * sizeOfTile;
